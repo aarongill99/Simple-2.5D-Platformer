17e541fcc63d98157faedf62f7680730a667eeb4
aarongill99 <55856823+aarongill99@users.noreply.github.com>
2019-10-21 14:34:47 -0700

    Added Jumping Physics
    Finished Jumping Physics along with gravity and double jump

---
+++ 2.5D Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    [SerializeField]
+    private float _jumpHeight = 15.0f;
+    private float _yVelocity;
+    [SerializeField]
+    private bool _doubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        if (_controller.isGrounded == true)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _doubleJump = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && _doubleJump == true)
+            {
+                _yVelocity += _jumpHeight * 1.3f;
+                _doubleJump = false;
+            }
+            _yVelocity -= _gravity;
+            
+        }
+
+        velocity.y = _yVelocity;
+
+        _controller.Move(velocity * Time.deltaTime);
+
+    }
+}
